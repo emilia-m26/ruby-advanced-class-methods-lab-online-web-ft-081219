58b8acec030c80bcdff36f130124f5aae1ed427c
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-07 00:53:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -105,8 +105,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-   result = song if self.find_by_name(name) == name 
-      result  
+   if self.find_by_name(name) == name 
     else
       self.create_by_name(name)
   end
